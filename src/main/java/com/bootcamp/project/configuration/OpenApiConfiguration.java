5d82599114400a1ed928da076d9291c477618b76
hana <hanaasugianto@gmail.com>
2026-09-14 13:21:46 +0700

    feat: add Spring Boot Actuator dependency and configure OpenAPI documentation for the API

--- src/main/java/com/bootcamp/project/configuration/OpenApiConfiguration.java
+++ src/main/java/com/bootcamp/project/configuration/OpenApiConfiguration.java
@@ -1,5 +1,29 @@
 package com.bootcamp.project.configuration;
 
-public class OpenApiConfiguration {
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenApiConfiguration {
+    @Bean
+    public OpenAPI springOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Sistem Transaksi dan Stok Barang API")
+                        .description("API untuk manajemen category, supplier, product, transaksi, dan reporting")
+                        .version("v1.0.0"));
+    }
+
+    @Bean
+    public GroupedOpenApi apiGroupA() {
+        return GroupedOpenApi.builder()
+                .group("Bootcamp API")
+                .pathsToMatch("/**")
+                .packagesToScan("com.bootcamp.project.controller")
+                .build();
+    }
 }
